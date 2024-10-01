a75559f7e5b902ee02b7da45707849c4e308011d
Tomasz Witwicki <tomasz.witwicki@protonmail.com>
2024-10-01 15:06:02 +0200

    add bind to duplicate line and preserve cursor location

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,5 @@ vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>g", ":Telescope live_grep<CR>")
 vim.keymap.set("n", "<leader>y", "\"+y") -- copy to system clipboard (normal mode)
 vim.keymap.set("v", "<leader>y", "\"+y") -- copy to system clipboard (visual mode)
+vim.keymap.set("n", "<leader>d", "m`yyp``j") -- duplicate line but leave cursor on same column
+vim.keymap.set("n", "<leader>D", "m`yyP``k") -- duplicate line but leave cursor on same column
