392fcf4eceecc0cc59ebd7cfc3671e75fa29354b
Tomasz Witwicki <tomasz.witwicki@protonmail.com>
2024-10-09 23:35:45 +0200

    add buffers keybind

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ' ' -- set leader to space
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>g", ":Telescope live_grep<CR>")
+vim.keymap.set("n", "<leader>s", ":Telescope buffers<CR>")
 vim.keymap.set("n", "<leader>y", "\"+y") -- copy to system clipboard (normal mode)
 vim.keymap.set("v", "<leader>y", "\"+y") -- copy to system clipboard (visual mode)
 vim.keymap.set("n", "<leader>d", "m`yyp``j") -- duplicate line but leave cursor on same column
