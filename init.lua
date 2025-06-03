a5924794c2c87a6d62bc62978b5d1e3c73572068
Tomasz Witwicki <tomasz.witwicki@protonmail.com>
2025-06-03 16:59:53 +0200

    add bash lsp

--- init.lua
+++ init.lua
@@ -70,13 +70,5 @@ lspconfig.yamlls.setup({
   },
 })
 
--- autocompletion
--- vim.api.nvim_create_autocmd('LspAttach', {
---   callback = function(ev)
---     local client = vim.lsp.get_client_by_id(ev.data.client_id)
---     if client:supports_method('textDocument/completion') then
---       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
---     end
---   end,
--- })
---
+-- bashls
+vim.lsp.enable 'bashls'
