856dd6f007811d14cc26a3a64fe487b0341eb7af
Tomasz Witwicki <tomasz.witwicki@protonmail.com>
2025-06-03 16:39:26 +0200

    add blink-cmp for better completion

--- init.lua
+++ init.lua
@@ -69,3 +69,14 @@ lspconfig.yamlls.setup({
     },
   },
 })
+
+-- autocompletion
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   callback = function(ev)
+--     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+--     if client:supports_method('textDocument/completion') then
+--       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+--     end
+--   end,
+-- })
+--
