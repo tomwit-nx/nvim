92aa792f319cf3e508b41a3e3f22a79bd6b596bc
Tomasz Witwicki <tomasz.witwicki@protonmail.com>
2025-06-29 20:36:58 +0200

    add fzf on current buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ' ' -- set leader to space
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>g", ":Telescope live_grep<CR>")
 vim.keymap.set("n", "<leader>s", ":Telescope buffers<CR>")
+vim.keymap.set("n", "<leader>q", ":Telescope current_buffer_fuzzy_find<CR>")
 vim.keymap.set("n", "<leader>y", "\"+y") -- copy to system clipboard (normal mode)
 vim.keymap.set("v", "<leader>y", "\"+y") -- copy to system clipboard (visual mode)
 vim.keymap.set("n", "<leader>d", "m`yyp``j") -- duplicate line but leave cursor on same column
