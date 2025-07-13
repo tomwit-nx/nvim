bf7f378dbd6179a933d58e70f2150dd8632f14e0
Tomasz Witwicki <tomasz.witwicki@protonmail.com>
2025-07-13 14:11:14 +0200

    use webdevicosn

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,8 +4,8 @@ return {
   ---@type oil.SetupOpts
   opts = {},
   -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 }
